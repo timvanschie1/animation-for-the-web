b89a3265f896f28ca0804f10c6b76c37b5259078
timvanschie1 <timvanschie1@gmail.com>
2024-02-23 20:48:03 +0100

    Added page transitioning demo

---
+++ other/page-transitioning/script.js
@@ -0,0 +1,13 @@
+const ctaEl = document.querySelector('.js-cta');
+
+if (ctaEl) {
+  ctaEl.addEventListener('click', e => {
+    e.preventDefault();
+    e.target.classList.add('cta--pressed');
+
+    setTimeout(() => {
+      window.location.href = e.target.href;
+    }, 30);
+
+  })
+}
